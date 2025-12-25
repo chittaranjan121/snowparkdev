d9f51ca855e5c7c01ac3b1c968668716805e3e28
chittaranjan121 <chittaranjan121@gmail.com>
2025-12-25 06:50:44 +0000

    qwqqqq

--- first_snowpark_project/deploy_snowpark_app.py
+++ first_snowpark_project/deploy_snowpark_app.py
@@ -1,15 +1,18 @@
 import sys
 import os
 
+# Path to snowpark project
 project_dir = sys.argv[1]
 os.chdir(project_dir)
 
 print("🔧 Building Snowpark project...")
-if os.system("snow snowpark build --connection default") != 0:
+build_status = os.system("snow snowpark build --connection default")
+if build_status != 0:
     raise RuntimeError("❌ Build failed")
 
 print("🚀 Deploying Snowpark project...")
-if os.system("snow snowpark deploy --replace --connection default") != 0:
+deploy_status = os.system("snow snowpark deploy --replace --connection default")
+if deploy_status != 0:
     raise RuntimeError("❌ Deploy failed")
 
 print("✅ Snowpark deployment completed successfully")
